84187f8afbbb673388f0c6852c3dc9df54531fa2
qinxiaowen <453000697@qq.com>
2019-01-15 19:08:10 +0800

    project view init

--- Jobs/GitHubJob.cs
+++ Jobs/GitHubJob.cs
@@ -9,7 +9,7 @@ namespace Andoromeda.Kyubey.Incubator.Jobs
 {
     public class GitHubJob : Job
     {
-        [Invoke(Begin = "2018-11-01 0:00", Interval = 1000*60 , SkipWhileExecuting = true)]
+        [Invoke(Begin = "2018-11-01 0:00", Interval = 1000 * 60 * 5, SkipWhileExecuting = true)]
         public void SyncTokensRepository(IConfiguration config, ILogger logger)
         {
             try
