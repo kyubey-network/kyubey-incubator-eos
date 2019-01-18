df2762f2f656ff4751e1407898430ac3fba94afc
LittleSound <464388324@qq.com>
2019-01-18 12:37:20 +0800

    web

--- ClientApp/components/incubator-list.vue
+++ ClientApp/components/incubator-list.vue
@@ -1,5 +1,36 @@
 <template>
-    <div class="container" style="height:300px;">
-        孵化器分页列表
+    <div class="container">
+        <div v-for="x in list.data.data">
+          <h1>{{ x.id }}</h1>
+          <p>{{x.introduction}}</p>
+        </div>
     </div>
 </template>
+
+<script>
+  export default {
+    computed: {
+    },
+
+    data() {
+      return {
+        list: null,
+        skip: 0,
+        take: 12,
+        lang: 'zh'
+      }
+    },
+
+    methods: {
+    },
+
+    async created() {
+      this.list = await this.$http.get(`/api/v1/lang/${this.lang}/Incubator/list?skip=${this.skip}&take=${this.take}`)
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
